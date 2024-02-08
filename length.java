fef0567b52310844138531658df37c7987dd0cac
PriyaDhas <mpriyad2001@gmail>
2024-02-08 09:49:03 +0530

    running

---
+++ length.java
@@ -0,0 +1,10 @@
+public class noelement
+    //Find the number of elements in the given array
+
+    {
+        public static void main(String[] args)
+        {
+            int a[]={12,23,34,45,5,10};
+            System.out.print(a.length);
+        }
+    }
